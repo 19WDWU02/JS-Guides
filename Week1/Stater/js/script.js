42a7dccf81b7b469267d49ddf940336225c94ec9
Richard Hpa <richard.hpa@hotmail.com>
2019-05-10 08:28:15 +1200

    started writing up guides

---
+++ Week1/Stater/js/script.js
@@ -0,0 +1,14 @@
+console.log("This is logging in the console.");
+
+// alert("This is an alert appearing in the browser");
+
+var a;
+a = 2;
+
+
+var days = ["Monday","Tuesday","Wednesday"];
+var tasks = [];
+
+for (var i = 0; i < days.length; i++) {
+    array[i]
+}
