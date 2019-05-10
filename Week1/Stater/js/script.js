ac4949e3e13e157e0899ffdeec9b6058d3b0632f
Simon <watsonsemon@gmail.com>
2019-05-10 15:15:04 +1200

    Added parseInt, for loops dimentional arrays and functions
    Could also do with some extra detail and examples

--- Week1/Stater/js/script.js
+++ Week1/Stater/js/script.js
@@ -9,10 +9,32 @@ var a;
 a = 2;
 
 var b = 3;
-
+var c = "This is a string";
 
 var days = ["Monday","Tuesday","Wednesday"];
 var tasks = [];
+
+
+if (b == 3) {
+    console.log("Congrats!")
+}
+
+if ((a+2) != 5) {
+    // Do somthing
+} else {
+    // Do somthing else
+}
+
+var d = 10;
+var result = d % 2;
+// result is now 0
+
+d = 25;
+result = d % 3;
+// result is now 1
+
+
+
 
 for (var i = 0; i < days.length; i++) {
     console.log(days[i]);
