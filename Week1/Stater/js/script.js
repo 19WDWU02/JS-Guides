7759f8738cc4404fdb37fee3298910d5fbf08f35
Simon <watsonsemon@gmail.com>
2019-05-10 13:14:28 +1200

    Added If Else and Operators, as well as syntax highlighting examples
    Done a little bit of extra sorting as well

--- Week1/Stater/js/script.js
+++ Week1/Stater/js/script.js
@@ -1,14 +1,19 @@
-console.log("This is logging in the console.");
+// Console and Alert
+console.log("This is logging in the console");
 
 // alert("This is an alert appearing in the browser");
 
+
+// Variables
 var a;
 a = 2;
+
+var b = 3;
 
 
 var days = ["Monday","Tuesday","Wednesday"];
 var tasks = [];
 
 for (var i = 0; i < days.length; i++) {
-    array[i]
+    console.log(days[i]);
 }
